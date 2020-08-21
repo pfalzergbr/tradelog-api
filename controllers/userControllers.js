925b07cc308e48d7e634f1c45ec08decd35974f3
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2020-08-21 13:22:09 +0100

    change dummy useControllers to real endpoints

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,21 +3,47 @@
 const User = require('../models/user')
 
 //Fetch a user from the database
-exports.getUser = (req, res) => {
-    const userId = req.params.id;
-    res.send(`This should fetch user with id ${userId}`)
+exports.getUser = async (req, res) => {
+    const _id = await req.params.id;
+    try {
+        const user = await User.find({_id})
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+
 }
 //Register a new user
-exports.registerUser = (req, res) => {
-    res.send('This should register a new user')
+exports.registerUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 }
 //Update user information
-exports.updateUser = (req, res) => {
-    const userId = req.params.id;
-    res.send(`This should update user information of user id ${userId}`)
+exports.updateUser = async (req, res) => {
+    const _id = req.params.id;
+    try {
+        //Finds the user and update based on request body. 
+        const user = await User.findByIdAndUpdate({_id}, req.body)
+        //Finds the update user
+        const newUser = await User.findOne({_id});
+        res.send(newUser)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
 }
 //Delete a user
-exports.deleteUser = (req, res) => {
-    const userId = req.params.id;
-    res.send(`This should delete the user with id ${userId}`)
+exports.deleteUser = async(req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user  = await User.findOneAndRemove({_id})
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,17 +8,12 @@ const router = express.Router();
 const userController = require('../controllers/userControllers')
 
 //Fetch a user from the database
-
 router.get('/:id', userController.getUser)
-
 //Register a new user
 router.post('/', userController.registerUser)
-
 //Update user information
-router.put('/:id', userController.updateUser)
-
+router.patch('/:id', userController.updateUser)
 //Delete a user
 router.delete('/:id', userController.deleteUser)
-
 
 module.exports = router;

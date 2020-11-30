337f167e1b4b5c9e71b8866cbafc22bccab5556b
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2020-11-30 19:17:00 +0000

    install postgres, add first pool, disconnect mongoose

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const Pool = require('pg').Pool
+
+const user = process.env.PSQL_USER;
+const password = process.env.PSQL_PASSWORD;
+const host = process.env.PSQL_PORT;
+const database = process.env.PSQL_DBNAME;
+
+const pool = new Pool({
+    user,
+    password,
+    host, 
+    database
+})
+
+module.exports = pool;

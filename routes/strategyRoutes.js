c44c643d3cba3d413216e3f369fbf17a3869ec2d
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2020-12-03 12:36:48 +0000

    add express validation to strategy routes

--- routes/strategyRoutes.js
+++ routes/strategyRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const { body } = require('express-validator');
-const { checkValidation } = require('../middleware/check-Validation');
+const { checkValidation } = require('../middleware/check-validation');
 const auth = require('../middleware/auth');
 const strategyControllers = require('../controllers/strategyControllers');
 const router = express.Router();
 
-// Create new Strategy - TODO: add validation
 router.get('/user/', auth, strategyControllers.getUserStrategies);
 // Get all strategies by account ID
 router.get(
@@ -13,14 +12,28 @@ router.get(
     auth,
     strategyControllers.getAccountStrategies,
 );
-router.post('/', auth, strategyControllers.createStrategy);
+// Create new Strategy - TODO: add validation
+router.post(
+    '/',
+    auth,
+    [
+        body('strategyName').not().isEmpty().trim(),
+        body('accountId').not().isEmpty().isString(),
+        body('description').not().isEmpty().isString(),
+    ],
+    checkValidation,
+    strategyControllers.createStrategy,
+);
 // Get one strategy
 router.get('/:strategyId', auth, strategyControllers.getStrategy);
 // Update a strategy - TODO: add validation
-router.patch('/:strategyId', auth, strategyControllers.updateStrategy);
+router.patch('/:strategyId',     [
+    body('strategyName').not().isEmpty().trim(),
+    body('description').not().isEmpty().isString(),
+],
+checkValidation,auth, strategyControllers.updateStrategy);
 // Delete a strategy
 router.delete('/:strategyId', auth, strategyControllers.deleteStrategy);
 // Get all strategies by user ID
-
 
 module.exports = router;

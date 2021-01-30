901a10d574f4f3408fedc58919cecff701c9b7e8
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-30 14:59:25 +0000

    send strategy name when creating a new trade

--- services/trade-service.js
+++ services/trade-service.js
@@ -1,15 +1,13 @@
 const tradeDb = require('../db/trade-db');
 const accountDb = require('../db/account-db');
 
-
 const calcTradeGain = (snapshotBalance, tradeAmount) => {
-  const relativeGain = ((tradeAmount / snapshotBalance) * 100).toFixed(2)
-  if (relativeGain === 'NaN'){
-    return 0
+  const relativeGain = ((tradeAmount / snapshotBalance) * 100).toFixed(2);
+  if (relativeGain === 'NaN') {
+    return 0;
   }
   return relativeGain;
-}
-
+};
 
 exports.formatTrade = tradeData => {
   const amount = parseFloat(tradeData.amount);
@@ -22,13 +20,22 @@ exports.formatTrade = tradeData => {
 };
 
 exports.createNewTrade = async tradeData => {
+  const { account, user_id, amount } = tradeData;
   const { balance, currency } = await accountDb.getSnapshotBalance(
-    tradeData.account,
-    tradeData.user_id,
+    account,
+    user_id,
   );
-  const relativeGain = calcTradeGain(balance, tradeData.amount);
-  const trade = await tradeDb.insertNewTrade(tradeData, balance, currency, relativeGain);
-  return trade;
+  const relativeGain = calcTradeGain(balance, amount);
+  const trade = await tradeDb.insertNewTrade(
+    tradeData,
+    balance,
+    currency,
+    relativeGain,
+  );
+  //TODO little hacky, try find a better solution
+  const {strategy_name} = await tradeDb.findTradeById(user_id, trade.trade.trade_id);
+
+  return {trade: {...trade.trade, strategy_name}, account: trade.account};
 };
 
 exports.getUserTrades = async userId => {
@@ -61,7 +68,11 @@ exports.getTradeById = async (userId, tradeId) => {
 
 exports.updateTradeStrategy = async (trade_id, userId, updates) => {
   if (updates.strategy) {
-    const updatedTrade = await tradeDb.changeTradeStrategy(trade_id, userId, updates.strategy);
+    const updatedTrade = await tradeDb.changeTradeStrategy(
+      trade_id,
+      userId,
+      updates.strategy,
+    );
 
     if (!updatedTrade) {
       const error = new Error();
@@ -71,8 +82,12 @@ exports.updateTradeStrategy = async (trade_id, userId, updates) => {
     }
 
     return updatedTrade;
-  } else if (updates.notes) { 
-    const updatedTrade = await tradeDb.changeTradeDescription(trade_id, userId, updates.notes);
+  } else if (updates.notes) {
+    const updatedTrade = await tradeDb.changeTradeDescription(
+      trade_id,
+      userId,
+      updates.notes,
+    );
 
     if (!updatedTrade) {
       const error = new Error();
@@ -82,9 +97,8 @@ exports.updateTradeStrategy = async (trade_id, userId, updates) => {
     }
     return updatedTrade;
   } else {
-    throw new Error;
+    throw new Error();
   }
-
 };
 
 exports.deleteTrade = async (trade_id, user_id) => {
@@ -98,5 +112,3 @@ exports.deleteTrade = async (trade_id, user_id) => {
   }
   return deletedTrade;
 };
-
-

06a7aa47dbdf282eb65945fddc8bde50330c9a91
Gabor Pfalzer <pfalzer@gmail.com>
2021-03-10 18:00:25 +0000

    add accounts migration

--- data/migrations/20210310173056_user.js
+++ data/migrations/20210310173056_user.js
@@ -1,11 +1,14 @@
 
-exports.up = function(knex) {
-  knex.schema.createTable('users', tbl => {
-      tbl.increments('user_id');
-      table.string('user_name')
+exports.up = (knex) => {
+  return knex.schema.createTable('users', tbl => {
+      tbl.uuid('user_id').primary();
+      tbl.string('user_name').notNullable();
+      tbl.string('email').notNullable();
+      tbl.string('password').notNullable();
+      tbl.timestamp('created_at', [defaultNow])
   })
 };
 
-exports.down = function(knex) {
-  
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('users');
 };
---
+++ data/migrations/20210310174453_accounts.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  knex.schema.table('accounts', table => {
+    table.uuid('account_id').primary()
+    table.string('account_name').notNullable()
+    table.enu('currency', ['usd', 'eur', 'gbp', 'jpy'], { useNative: true, enumName: 'currency_type'}).notNull()
+    table.text('description')
+    table.decimal('balance').notNullable().defaultTo(0)
+    table.decimal('opening_balance').notNullable().defaultTo(0)
+    table.foreign('user_id').references('user_id').inTable('users').onDelete('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  
+};
